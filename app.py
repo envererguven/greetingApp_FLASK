731a39ecc9902524542803be3140b7c617009fe1
envererguven <44556597+envererguven@users.noreply.github.com>
2022-06-11 02:09:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	flash("Hi " + str(request.form['name_input']) + ", hoş geldin!")
 	return render_template("index.html")
